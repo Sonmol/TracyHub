49a8148cea528041f051df0d0b076c7c962208fc
Sonmol <85048287+Sonmol@users.noreply.github.com>
2022-08-02 06:26:45 -0500

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,20 @@
+--[[
+
+// TracyHub v1 \\
+
+Credits:
+[Sonmol]: Owner, developer, BigPaintball
+
+Kocmoc is owned, coded developed and managed by Sonmol alone. Anyone else who claims to be a developer is an impersonator.
+
+Thanks for using TracyHub! ^-^
+
+]]
+
+if game.PlaceId == 3527629287 then
+	loadstring(game:HttpGet(""))()
+elseif game.PlaceId == 5956785391 or game.PlaceId == 7722306047 then
+	game.Players.LocalPlayer:Kick("Coming Soon.")
+else
+	game.Players.LocalPlayer:Kick("Error loadng TracyHub.\n\nNo supported game found.\n\nPlease join a supported game.")
+end
